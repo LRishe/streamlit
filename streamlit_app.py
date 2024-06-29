4ad01c041d24036850c12c45ce28ba2bf7fbeed6
LRishe <77412099+LRishe@users.noreply.github.com>
2024-06-28 22:26:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if category=="Technology":
         ("Phones", "Accessories", "Machines", "Copiers")
     )
 filtered_data = df[(df["Category"] == category) & (df["Sub_Category"].isin(subcategories))]
-st.line_chart(filtered_data["Sales"])
+st.line_chart(filtered_data)
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
