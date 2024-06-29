71deb9f3eb5f810f51550f5124fd498da7f75f39
LRishe <77412099+LRishe@users.noreply.github.com>
2024-06-28 21:35:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,12 @@ if category=="Technology":
         ("Phones", "Accessories", "Machines", "Copiers")
     )
 
+ if selected_subcategories:
+    sales_data = Sales[category, subcategories]
+    profit_data = Profitcategory, subcategories]
+
+st.line_chart(sales_data)
+
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
